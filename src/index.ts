1970039ec117552c156f815583fa50f4e7942771
DreadArceus <66964119+DreadArceus@users.noreply.github.com>
2021-10-27 18:39:21 +0530

    YEP (#1)

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import ormConfig from "./orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { CourseResolver } from "./resolvers/course";
+import cors from "cors";
+import { MyContext } from "./types";
+
+const main = async () => {
+  const conn = await createConnection(ormConfig);
+  conn.runMigrations();
+
+  const app = express();
+
+  app.use(
+    cors({
+      origin: "https://studio.apollographql.com",
+      credentials: true,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [CourseResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ req, res }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("yep");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ src/orm.config.ts
@@ -0,0 +1,13 @@
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { Course } from "./entities/Course";
+
+export default {
+  entities: [Course],
+  type: "postgres",
+  database: "potatoDB",
+  username: "postgres",
+  password: "11037",
+  logging: !__prod__,
+  synchronize: true,
+} as Parameters<typeof createConnection>[0];
