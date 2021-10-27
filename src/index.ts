0c4bb81fc2f6d4cc097658b49d247339bdfd5474
shivral somani <shivral312002@gmail.com>
2021-10-27 15:27:18 +0530

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { Course } from "./entities/course";
+const main=async()=>{
+
+    const con=await createConnection({
+        database:"potatoDB",
+        user:'postgres',
+        password:'Shivral31',
+        type:'postgres',
+        debug:!__prod__,
+        entities:[Course]
+    });
+};
+main();
 console.log("jesus lodu ")
