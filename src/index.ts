61d78af6b21b33d8a27846e238cecea2997dc135
shivral somani <shivral312002@gmail.com>
2021-10-27 18:40:07 +0530

    lol

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
+import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { __prod__ } from "./constants";
 import { Course } from "./entities/course";
-const main=async()=>{
-
-    const con=await createConnection({
-        database:"potatoDB",
-        user:'postgres',
-        password:'Shivral31',
-        type:'postgres',
-        debug:!__prod__,
-        entities:[Course]
-    });
+import ormConfig from "./orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { CourseResolver } from "./resolvers/course";
+import  cors from "cors";
+import { MyContext } from "./types";
+const main = async () => {
+  const con = await createConnection(ormConfig);
+  con.runMigrations();
+  const app = express();
+  app.use(cors({origin:"https://studio.apollographql.com",credentials:true}))
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [CourseResolver], // views of DJango copy
+      validate: false
+    }), 
+    context:({req,res}):MyContext=>({req,res}),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app,cors:false });
+  app.listen(4000, () => {
+    console.log("yes logging ");
+  });
 };
 main();
-console.log("jesus lodu ")
+console.log("jesus lodu ");
