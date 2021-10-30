158dbe3821b50c3f4f17f9a9f25476bbb5241d6d
SouhardyaMalakar <71064829+SouhardyaMalakar@users.noreply.github.com>
2021-10-30 22:23:15 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -48,8 +48,8 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
         httpOnly: true,
-        sameSite: "lax",
-        secure: true,
+//         sameSite: "lax",
+//         secure: true,
       },
       secret: "somesecretbro",
       resave: false,
